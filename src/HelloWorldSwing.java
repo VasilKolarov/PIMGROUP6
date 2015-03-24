3132de4a374a0534564db9ac0decb169aeaf2e8d
Jackevansevo <jack@evans.gb.net>
2015-03-24 14:11:41 +0000

    Added base files for eclipse setup

---
+++ src/HelloWorldSwing.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class HelloWorldSwing {
+
+	private static void createAndShowGUI() {
+		// Create frame and set up the window
+		JFrame frame = new JFrame("HelloWorldSwing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Add hello world label
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+		
+		// Display the window
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread;
+		// creating and showing this application's GUI
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+}
