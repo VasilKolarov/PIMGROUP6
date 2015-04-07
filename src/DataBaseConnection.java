dd637794997fbb8b43a3271b496e9a6a2673b4c8
Jackevansevo <jack@evans.gb.net>
2015-04-07 11:47:29 +0100

    Refactor Code

--- src/DataBaseConnection.java
+++ src/DataBaseConnection.java
@@ -1,8 +1,8 @@
 import org.apache.ibatis.jdbc.ScriptRunner;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.sql.*;
 import java.util.Scanner;
@@ -40,25 +40,16 @@ public class DataBaseConnection {
 			// If the database doesn't exist locally then we need to create it
 			if (!DBExists) {
 				createDataBase();
-			} System.out.println("Database already exists");
 
+				// Populate the database with test data
+				populateDataBase();
+			}
 			// Connect to the new Database
 			PIMConnection = DriverManager.getConnection(PIMDB_URL, USER, PASS);
 			System.out.println("Connected to Database");
 
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if(MysqlConnection != null) {
-					MysqlConnection.close();
-				}
-				if(PIMConnection != null) {
-					PIMConnection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
@@ -66,73 +57,68 @@ public class DataBaseConnection {
 	 *  Creates the DataBase if it doesn't already exist
 	 */
 	public void createDataBase() {
-		Statement stmt;
 		try {
 			// Create the Database
-			stmt = MysqlConnection.createStatement();
+			Statement stmt = MysqlConnection.createStatement();
 			stmt.executeUpdate("CREATE DATABASE PIMDataBase");
-			System.out.println("Successfully created Database");
+			stmt.close();
 
 			// Make a connection to the newly created DataBase
 			PIMConnection = DriverManager.getConnection(PIMDB_URL, USER, PASS);
 
-			// Create the Database tables by running an sql script
+			// Set up ScriptRunner and Reader to read the .sql files
 			String currentDir = System.getProperty("user.dir");
-			String script;
-			script = currentDir + "CreateDBTables.sql";
+			String script = currentDir + "/CreateDBTables.sql";
+			ScriptRunner scriptRunner = new ScriptRunner(PIMConnection);
+			Reader reader = new BufferedReader(new FileReader(script));
 
-            // Initialise object for ScripRunner
-            ScriptRunner sr = new ScriptRunner(PIMConnection);
+			// Create Database tables by executing the sql script
+			scriptRunner.runScript(reader);
 
-            // Give the input file to Reader
-            Reader reader = new BufferedReader(new FileReader(script));
-
-            // Execute the script
-            sr.runScript(reader);
-
-			System.out.println("Successfully created Database tables");
-
-			// Ask if user wishes to automatically populate the database with test data
-			Scanner input = new Scanner(System.in);
-            System.out.println("Run population script [Y/n]?");
-            String a = input.next();
-            if(a.equalsIgnoreCase("y")) {
-				populateDataBase();
-			}
-		} catch (SQLException | FileNotFoundException e) {
+			// Close script runner and reader
+			scriptRunner.closeConnection();
+			reader.close();
+		} catch (SQLException | IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if(PIMConnection != null) {
-					PIMConnection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
 	/**
-	 * Runs a populate database script to fill the database with sample data
+	 * Runs a populate database script to fill the Database with sample data
 	 */
 	public void populateDataBase() {
-		// Create the Database tables by running an sql script
-		String currentDir = System.getProperty("user.dir");
-		String script;
-		script = currentDir + "PopulateDBWithTestData.sql";
+		// Check with the user if they want to populate the Database with sample data
+		Scanner input = new Scanner(System.in);
+		System.out.println("Run population script [Y/n]?");
+		String a = input.next();
+		if(a.equalsIgnoreCase("y")) {
+			try {
+				PIMConnection = DriverManager.getConnection(PIMDB_URL, USER, PASS);
 
-		// Initialise object for ScripRunner
-		ScriptRunner sr = new ScriptRunner(PIMConnection);
+				// Create the Database tables by running an sql script
+				String currentDir = System.getProperty("user.dir");
+				String populationScript = currentDir + "/PopulateDBWithTestData.sql";
 
-		// Give the input file to Reader
-		Reader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader(script));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+				// Initialise object for ScripRunner
+				ScriptRunner sr = new ScriptRunner(PIMConnection);
 
-		// Execute the script to populate the DataBase
-		sr.runScript(reader);
+				// Give the input file to Reader
+				try (Reader reader = new BufferedReader(new FileReader(populationScript))) {
+					// Execute the script to populate the DataBase
+					sr.runScript(reader);
+				} catch (IOException e) {
+					System.out.println("Unable to execute populate script");
+				}
+			} catch (SQLException e) {
+				System.out.println("Unable to connect to the Database");
+			}
+		} else System.out.println("Population script was not run");
+	}
+
+	/**
+	 * Returns a results set of all the rows form a given table
+	 */
+	public void getTableData(String table) {
+		System.out.println("This is awesome" + table);
 	}
 }
